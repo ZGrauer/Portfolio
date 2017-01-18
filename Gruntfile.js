1434b96dcb24e892b284e8b82d88ec020820ec45
ZGrauer <zgrauer@wgu.edu>
2017-01-18 15:04:56 -0600

    feat: Add project skills.  Add Grunt Watch
    Add skills to each project in index.html
    Add Grunt Watch to auto-run tasks cssmin and imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,35 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    cssmin: {
+    cssmin: {                           // Minify all *.CSS files
       target: {
         files: [{
-          expand: true,
-          cwd: 'css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'css',
-          ext: '.min.css'
+          expand: true,                 // Enable dynamic expansion
+          cwd: 'src/css',               // Source files in ./src/css/
+          src: ['*.css', '!*.min.css'], // Look for all non-miified CSS
+          dest: 'css',                  // Send output to ./css/
+          ext: '.min.css'               // Add min to filename
         }]
       }
     },
-    imagemin: {                          // Task
+    imagemin: {                          // Minify images
       dynamic: {                         // Another target
         files: [{
           expand: true,                  // Enable dynamic expansion
-          cwd: 'src/',                   // Src matches are relative to this path
+          cwd: 'src/images/',            // Src matches are relative to this path
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-          dest: 'images/'                  // Destination path prefix
+          dest: 'images/'                // Destination path prefix
         }]
+      }
+    },
+    watch: {                                  // Watch for file changes and run task
+      css: {
+        files: ['src/css/*.css'],             // Minify when CSS sources change
+        tasks: ['cssmin']
+      },
+      images: {
+        files: ['src/images/*.{png,jpg,gif}'],  // Minify when images sources change
+        tasks: ['imagemin']
       }
     }
   });
@@ -35,5 +45,8 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['cssmin', 'imagemin']);
+
+  // Run tasks on file change
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
--- README.md
+++ README.md
@@ -16,22 +16,36 @@ A responsive website to display completed portfolio projects, along with work ex
 5. Edit HTML and CSS files as needed.
 
 ### Development
-Modify the site as needed but be sure to edit the non-minified files. Edit the files that do not contain *min* in their names and create the *min* versions using Grunt. Additionally, save raw, large images to the *src* folder and use grunt to minify them for you.
+Modify the site as needed but be sure to edit the non-minified CSS and image files. Only modify CSS files in `.\src\css` and image files in `.\src\images`. Save raw, large images to the `.\src\images` folder and use grunt to minify them for you.
 
-Open your favorite Terminal and run these commands.
-```sh
-$ grunt cssmin
-```
+[Grunt Watch](https://github.com/gruntjs/grunt-contrib-watch#compiling-files-as-needed) is setup to monitor these folders for changes and auto run the `cssmin` and `imagemin` tasks.  To start Grunt Watch open your favorite Terminal and run this command:
 
 ```sh
-$ grunt imagemin
+$ grunt watch
 ```
 
-`cssmin` creates **.min.css* versions for all the files within the *css* folder.
-`imagemin` creates new, optimized versions of all images in the *src* folder. The new images are saved to folder *images*.
+Grunt should then be watching for file changes.  The output should look similar to this:
+
+```sh
+$ grunt watch
+Running "watch" task
+Waiting...
+>> File "src\css\style.css" changed.
+Running "cssmin:target" (cssmin) task
+>> 3 files created. 176.68 kB → 143.6 kB
+
+Done.
+```
+
+You can stop Grunt Watch by pressing <kbd>CTRL</kbd>+<kbd>C</kbd>
+
+```sh
+$ ^CTerminate batch job (Y/N)? Y
+```
+
 
 ### Todos
-- [ ] add 2 additional projects, replacing Project 2 and Project 3
+- [X] add 2 additional projects, replacing Project 2 and Project 3
 - [ ] hover effect for project thumbnail
 - [X] Zocial GitHub and LinkedIn buttons in Contact aside
 
